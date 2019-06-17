2fe3e0b85c47f69afb905a72963b3aeadf3c42e8
Tyson Morris <temorris87@yahoo.com>
2019-06-17 14:12:02 -0400

    Created controlled component for search.

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -4,26 +4,49 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 
-function Search(props) {
-  return (
-    <Paper className="search"
-           elevation="3"
-           component="div"
-    >
-      <TextField
-        label="UPC"
-        type="text"
-        name="upc"
-        margin="normal"
-        variant="outlined"
-        autoFocus={true}
-        fullWidth={true}
-      />
-      <Button variant="contained" color="primary">
-        Search
-      </Button>
-    </Paper>
-  )
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      upc: ""
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render() {
+    return (
+      <Paper className="search"
+             elevation="3"
+             component="div"
+      >
+        <TextField
+          label="UPC"
+          type="text"
+          name="upc"
+          margin="normal"
+          variant="outlined"
+          autoFocus={true}
+          fullWidth={true}
+          onChange={this.handleInput}
+          value={this.state.upc}
+        />
+        <Button variant="contained" color="primary">
+          Search
+        </Button>
+      </Paper>
+    )
+  }
 }
 
 export default Search;
