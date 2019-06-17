34a92481f124f4964cf1800968187443d9002c95
Malik Covington <malikcovington@yahoo.com>
2019-06-17 10:55:09 -0400

    added bolierplate to Add Item Component

---
+++ client/components/AddUpdateProduct.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {Route, Link, Redirect} from 'react-router-dom'
+import axios from 'axios'
+
+class AddItem extends Component {
+
+    
+
+
+}
+
+export default AddItem 
