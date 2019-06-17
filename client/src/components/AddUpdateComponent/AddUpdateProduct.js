45d3510ae5e28f91ccb1b4dbe576a4ec44d0544f
Malik Covington <malikcovington@yahoo.com>
2019-06-17 13:37:24 -0400

    updated layout of add_item_Component

--- client/src/components/AddUpdateComponent/AddUpdateProduct.js
+++ client/src/components/AddUpdateComponent/AddUpdateProduct.js
@@ -3,13 +3,12 @@ import {Route, Link, Redirect} from 'react-router-dom'
 import axios from 'axios'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import './AddUpdate.css'
 
 class AddItem extends Component {
   constructor(props){
     super(props)
-
-
-
 
     }
 
@@ -17,50 +16,62 @@ class AddItem extends Component {
     render(){
         return(
 
-            <div>
-            <div><h2>Add an Item</h2></div>
+            
+                <div className = "add-update-product">
+                <Paper
+                className = "paper"
+                component = "div"
+                elevation = "3">
+                <div><h2>Add an Item</h2></div>
+
+
+                
+                <TextField 
+                className="outlined-with-placeholder"
+                label="Brand"
+                placeholder="Brand"
+                variant="outlined"
+                margin="normal"
+                />
+
+                <TextField 
+                className="outlined-with-placeholder"
+                label="Product Name"
+                placeholder="Name"
+                variant="outlined"
+                margin="normal"
+                />
+
+                <TextField 
+                className="outlined-with-placeholder"
+                label="Product Description"
+                placeholder="Product Description"
+                variant="outlined"
+                margin="normal"
+                />
+
+                <TextField 
+                className="outlined-with-placeholder"
+                label="Category"
+                placeholder="Category"
+                variant="outlined"
+                margin="normal"
+                />
+                
+
+                <div>
+                    <Button 
+                    className = "submit-button"
+                    variant="contained" 
+                    color="primary">
+                    Submit
+                    </Button>
+                </div>
+
+                </Paper>
+                </div>
 
             
-            <TextField 
-            id="outlined-with-placeholder"
-            label="Brand"
-            placeholder="Brand"
-            variant="outlined"
-            margin="normal"
-            />
-
-            <TextField 
-            id="outlined-with-placeholder"
-            label="Product Name"
-            placeholder="Name"
-            variant="outlined"
-            margin="normal"
-            />
-
-            <TextField 
-            id="outlined-with-placeholder"
-            label="Product Description"
-            placeholder="Product Description"
-            variant="outlined"
-            margin="normal"
-            />
-
-            <TextField 
-            id="outlined-with-placeholder"
-            label="Category"
-            placeholder="Category"
-            variant="outlined"
-            margin="normal"
-            />
-
-            <div>
-        <Button variant="contained" color="primary">
-        Submit
-        </Button>
-            </div>
-
-
-            </div>
         )
 
 
